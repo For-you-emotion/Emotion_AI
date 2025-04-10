f4564888431c5d31b49815552783267a7b5fd9da
wwwxsv19 <wwwxsv19@gmail.com>
2025-04-10 15:51:44 +0000

    feat: .wav 이름으로  가져오기 / move: wav.py 분리

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from dto import Request 
 from db import get_db
 from feedback import saveData, feedback
+from wav import save, find
 
 import logging, os, sys
 
@@ -15,22 +16,22 @@ def read_root() :
     return {'Hello Emotion!'}
 
 @app.post("/wavTest")
-async def getWavFile(file: UploadFile = File(...)) :
-    logging.info(f"작업 : {sys.path}")
-    logging.info(".wav 파일 주고받기 테스트")
+async def saveWav(file: UploadFile = File(...)) :
+    try :
+        return save(file)
+    except HTTPException as httpEx :
+        raise httpEx
+    except Exception as e :
+        raise HTTPException(status_code = 500, detail = str(e))
 
-    if not file.filename.endswith('.wav') :
-        raise HTTPException(status_code = 400, detail = ".wav 파일 형식이 다릅니다!")
-    
-    filePath = os.path.join("wavFiles", file.filename)
-
-    with open(filePath, "wb") as buffer :
-        buffer.write(await file.read())
-
-    logging.info(f"정상적으로 파일을 받아 저장하였습니다! : {file.filename}")
-
-    return FileResponse(filePath, media_type = 'audio/wav', filename = file.filename)
-
+@app.get("/{text}")
+def getWav(text: str): 
+    try :
+        return find(text)
+    except HTTPException as httpEx :
+        raise httpEx
+    except Exception as e :
+        raise HTTPException(status_code = 500, detail = str(e))
 
 @app.post("/feedback")
 async def callFeedbackAI(request: Request, db: AsyncSession = Depends(get_db)) :
---
+++ wav.py
@@ -0,0 +1,32 @@
+from fastapi import UploadFile, File
+from fastapi.responses import FileResponse
+
+import logging, os
+
+async def save(file: UploadFile) :
+    logging.info(".wav 파일 주고받기 테스트")
+
+    if not file.filename.endswith('.wav') :
+        raise HTTPException(status_code = 400, detail = ".wav 파일 형식이 다릅니다!")
+
+    filePath = os.path.join("wavFiles", file.filename)
+
+    if os.path.exists(filePath) :
+        raise HTTPException(status_code = 409, detail = "동일한 이름의 .wav 파일이 존재합니다!")
+
+    with open(filePath, "wb") as buffer :
+        buffer.write(await file.read())
+
+    logging.info(f"정상적으로 파일을 받아 저장하였습니다! : {file.filename}") 
+
+    return FileResponse(filePath, media_type = 'audio/wav', filename = file.filename)
+
+async def find(text: str) :
+    logging.info(".wav 파일 이름으로 가져 오기")
+    
+    filePath = os.path.join("wavFiles", str(text) + ".wav")
+
+    if not os.path.exists(filePath) :
+        raise HTTPException(status_code = 404, detail = "존재하지 않는 .wav 파일입니다!")
+    
+    return FileResponse(filePath, media_type = 'audio/wav', filename = text)
