8a89e56b19e5058ad4d826aa6bb8a9d526de2a94
wwwxsv19 <wwwxsv19@gmail.com>
2025-04-23 08:51:57 +0000

    remove: 불필요한 API 및 패키지, 주석 삭제

--- main.py
+++ main.py
@@ -3,26 +3,12 @@ from fastapi.responses import FileResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db import get_db
-from feedback import saveData, findData #, feedback
-from wav import save, find, delete
+from feedback import saveData, findData, feedback
+from wav import save, find
 
 import logging, os, sys
 
 app = FastAPI()
-
-@app.get("/")
-def read_root() :
-    return {"Hello Emotion!"}
-
-# wav 저장 후 그대로 반환
-@app.post("/wavTest")
-async def saveWav(file: UploadFile = File(...)) :
-    try :
-        return await save(file)
-    except HTTPException as httpEx :
-        raise httpEx
-    except Exception as e :
-        raise HTTPException(status_code = 500, detail = str(e))
 
 # 파일 이름으로 wav 반환
 @app.get("/wav/{num}")
@@ -38,25 +24,7 @@ async def getWav(num: str) :
 @app.get("/feelings/{num}")
 async def getFeelings(num: str, db: AsyncSession = Depends(get_db)) :
     try :
-        return await findDate(num, db)
-    except HTTPException as httpEx :
-        raise httpEx
-    except Exception as e :
-        raise HTTPException(status_code = 500, detail = str(e))
-
-@app.post("/feedback/{hwId}/{beadNum}")
-async def callFeedbackAI(
-        hwId: str, beadNum: int, file: UploadFile = File(...), 
-        db: AsyncSession = Depends(get_db)
-    ) :
-    try :
-        fileName = await save(file)
-
-        # result = feedback(request)
-        
-        await saveData(hwId, beadNum, fileName, db)
-
-        return fileName
+        return await findData(num, db)
     except HTTPException as httpEx :
         raise httpEx
     except Exception as e :
